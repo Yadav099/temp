0895656ddee63141916e9be6a0c9e7da607a7e86
Yadav Padiyar M <yadav.padiyar@hashedin.com>
2020-03-11 12:32:29 +0530

    Worked on utility functions
    performed authentication of user

--- utils/login.py
+++ utils/login.py
@@ -1,5 +1,5 @@
-import app
-from app import APP
+
+from app import APP ,bcrypt
 from flask import request, Response
 import json
 
@@ -7,15 +7,36 @@ from app.models import Employee, Company
 
 
 def login_get_user(user_data):
+
     try:
         login_data = {
             "company_name": user_data['companyName'],
             "emp_email": user_data['userName'],
             "emp_pass": user_data['password'],
         }
-        employee = Employee.query.filter_by(emp_pass=login_data["emp_pass"], emp_email=login_data["emp_email"]).first()
+
+# connecting to employee and company database
+        employee = Employee.query.filter_by(emp_email=login_data["emp_email"]).first()
         company = Company.query.filter_by(company_name=login_data["company_name"]).first()
-        if employee.id and company.id:
-            return Response("", 200)
+        print(employee.emp_pass)
+        print("aaaa")
+        print(bcrypt.generate_password_hash(login_data["emp_pass"]))
+
+        # function to check  whether email and company name are vlid or not
+        if company.company_name == login_data["company_name"]:
+            if employee.emp_email == login_data["emp_email"]:
+
+# condition statement to check the password with stored hashed password
+              if employee and bcrypt.check_password_hash(employee.emp_pass, login_data["emp_pass"]):
+                return "Successful"
+              else:
+                return "3"
+            else:
+                return "2"
+        else:
+              return "1"
+
+
     except Exception as e:
-        return Response("", 400)
+        print(str(e))
+

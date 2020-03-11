1505e394c7ded121838f01dedf4f7f2037f56765
Yadav Padiyar M <yadav.padiyar@hashedin.com>
2020-03-11 19:31:16 +0530

    Solving merge conflict

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
+
 
 def create_app(config_name):
     APP = Flask(__name__, instance_relative_config=True)
     APP.config.from_object(app_config[config_name])
     APP.debug = True
+    CORS(APP)
     return APP
 
 
